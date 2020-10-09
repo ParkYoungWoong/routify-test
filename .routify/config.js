d107e9e59f01c58aaed68c55fd2d6fd4a2b384bf
ParkYoungWoong <thesecon@gmail.com>
2020-10-09 19:49:49 +0900

    Test...

---
+++ .routify/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "pages": "src/pages",
+  "sourceDir": "public",
+  "routifyDir": ".routify",
+  "ignore": "",
+  "dynamicImports": true,
+  "singleBuild": false,
+  "noHashScroll": false,
+  "extensions": [
+    "svelte",
+    "html",
+    "svx",
+    "md"
+  ],
+  "distDir": "dist",
+  "hashScroll": true,
+  "started": "2020-10-09T10:34:19.138Z"
+}
